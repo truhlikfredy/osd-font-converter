1fe99d139a7cfe07247df3a1d44b31f2a0922f90
Anton Krug <antonkrug@gmail.com>
2018-10-25 03:07:34 +0100

    Having own command line options

--- src/main/java/eu/antonkrug/Converter.java
+++ src/main/java/eu/antonkrug/Converter.java
@@ -1,10 +1,6 @@
 package eu.antonkrug;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import eu.antonkrug.model.Filter;
-import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import picocli.CommandLine;
 
 import java.io.File;
 import java.util.logging.Level;
@@ -13,21 +9,41 @@ import java.util.logging.Level;
  * @author Anton Krug on 24/10/18
  * @version v0.1
  */
-public class Converter {
+@CommandLine.Command(name = "osd-font-converter.jar", header = "%n@|green Batch converter for OSD fonts with \"|@@|underline post-processing filters|@@|green \"(tm) :-)|@")
+public class Converter implements Runnable {
+  @CommandLine.Option(names = "-a", description = "apply all filters from bundled resources")
+  boolean create;
+
+  @CommandLine.Option(names = { "-f", "--filter" }, paramLabel = "FILTER", description = "How much of log verbosity you want INFO, SEVERE, OFF")
+  String filter;
+
+  @CommandLine.Option(names = { "-l", "--logLevel" }, paramLabel = "LOG_LEVEL", description = "give path to your filter.yml description (to reference bundled filter use \"resource:/outline-2x\") ")
+  String logLevel;
+
+  @CommandLine.Parameters(paramLabel = "FONTS", description = "one ore more files containing fonts")
+  File[] fonts;
+
+  @CommandLine.Option(names = { "-h", "--help" }, usageHelp = true, description = "display a help message")
+  private boolean helpRequested;
 
   public static void main(String[] args) throws Exception {
-    System.out.println("Converter started...");
-
-//    LoggerHandler.setLevel(Level.INFO);
-
-    Font font = new FontMcm("/home/fredy/Desktop/a");
-    font.load();
-
-    Font fontWithEffect = font.applyFilter("resource:/outline-2x");
-
-    Font fontPng = new FontPng(fontWithEffect);
-    fontPng.save();
+    CommandLine.run(new Converter(), args);
   }
 
+  @Override
+  public void run() {
+    Level level = Level.parse(logLevel);
+    if (level != null) {
+      LoggerHandler.setLevel(Level.INFO);
+    }
 
+    //    Font font = new FontMcm("/home/fredy/Desktop/a");
+    //    font.load();
+    //
+    //    Font fontWithEffect = font.applyFilter("resource:/outline-2x");
+    //
+    //    Font fontPng = new FontPng(fontWithEffect);
+    //    fontPng.save();
+    System.out.println("Hello, ");
+  }
 }
