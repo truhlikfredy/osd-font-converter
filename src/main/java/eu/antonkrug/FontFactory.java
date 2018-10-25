e9e67d409b5650b53562a7a502124db709c26a21
Anton Krug <antonkrug@gmail.com>
2018-10-25 05:57:30 +0100

    Give correct implementation of font depending on the full path

---
+++ src/main/java/eu/antonkrug/FontFactory.java
@@ -0,0 +1,37 @@
+package eu.antonkrug;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.apache.commons.io.FilenameUtils.getExtension;
+import static org.apache.commons.io.FilenameUtils.removeExtension;
+
+/**
+ * @author Anton Krug on 25/10/18
+ * @version v0.1
+ */
+public class FontFactory {
+
+  private static final Logger LOGGER = LoggerHandler.getLogger(FontFactory.class.getName());
+
+
+  public static Font getInstance(String fullPath) {
+    final String path      = removeExtension(fullPath);
+    final String extension = getExtension(fullPath);
+
+    LOGGER.log(Level.FINEST, "Fullpath=" + fullPath + " path=" + path + " extension=" + extension);
+    switch (extension) {
+      case "mcm":
+        return new FontMcm(path);
+
+      case "png":
+        return new FontPng(path);
+
+      default:
+        LOGGER.log(Level.SEVERE, "Extension " + extension + " is not supported as font format.");
+        return null;
+    }
+  }
+
+
+}
