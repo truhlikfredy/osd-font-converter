1231f6007ffaaef88cff8513ec4a8c2a6f5062a3
Anton Krug <antonkrug@gmail.com>
2018-10-26 05:40:24 +0100

    Lowering complexity metric by removing case switch statement

--- src/main/java/eu/antonkrug/FontFactory.java
+++ src/main/java/eu/antonkrug/FontFactory.java
@@ -1,5 +1,9 @@
 package eu.antonkrug;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,22 +18,38 @@ public class FontFactory {
 
   private static final Logger LOGGER = LoggerHandler.getLogger(FontFactory.class.getName());
 
+  private static final Map<String, Function<String, Font>> EXTENSION_MAP;
+
+  static {
+    Map<String, Function<String, Font>> map = new ConcurrentHashMap<>();
+
+    // return is implied in this case, and function is consumer and supplier in one
+    // https://stackoverflow.com/questions/38540481/return-value-by-lambda-in-java
+    // https://stackoverflow.com/questions/31251629/java-8-supplier-with-arguments-in-the-constructor
+    map.put("mcm", path -> new FontMcm(path));
+    map.put("png", path -> new FontPng(path));
+
+    // similar topics:
+    // https://dzone.com/articles/4-modules-of-spring-architecture
+    // https://stackoverflow.com/questions/28417262/java-8-supplier-consumer-explanation-for-the-layperson
+
+    // Making the map unmodifiable
+    EXTENSION_MAP = Collections.unmodifiableMap(map);
+  }
+
 
   public static Font getInstance(String fullPath) {
     final String path      = removeExtension(fullPath);
-    final String extension = getExtension(fullPath);
+    final String extension = getExtension(fullPath).toLowerCase();
 
     LOGGER.log(Level.FINEST, "Fullpath=" + fullPath + " path=" + path + " extension=" + extension);
-    switch (extension) {
-      case "mcm":
-        return new FontMcm(fullPath);
 
-      case "png":
-        return new FontPng(fullPath);
-
-      default:
-        LOGGER.log(Level.SEVERE, "Extension " + extension + " (taken from " + fullPath + ") is not supported as font format.");
-        return null;
+    if (EXTENSION_MAP.containsKey(extension)) {
+      return EXTENSION_MAP.get(extension).apply(fullPath);
+    }
+    else{
+      LOGGER.log(Level.SEVERE, "Extension " + extension + " (taken from " + fullPath + ") is not supported as font format.");
+      return null;
     }
   }
 
