cd3e8a598b45000687acba7a5367bbc3aa4cf763
Anton Krug <antonkrug@gmail.com>
2018-10-27 23:22:17 +0100

    Checking for changes in meantime, safer checkout, directly pushing without setting remote

--- tools/push-updated-readme.sh
+++ tools/push-updated-readme.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
-# Based on: https://gist.github.com/willprice/e07efd73fb7f13f917ea
+# Based on:
+# https://gist.github.com/willprice/e07efd73fb7f13f917ea
+# https://github.com/StoPlay/stoplay-ext/blob/fb0c721e69c48646f77cce98e2b5f6f83854cab2/.travis/push.sh
+# https://gist.github.com/mitchellkrogza/a296ab5102d7e7142cc3599fca634203
 
 REPO="truhlikfredy/osd-font-converter"
+
+
+check_if_repo_changed() {
+    local head_ref branch_ref
+
+    head_ref=$(git rev-parse HEAD)
+    if [[ $? -ne 0 || ! $head_ref ]]; then
+        err "failed to get HEAD reference"
+        exit 1
+    fi
+
+    branch_ref=$(git rev-parse "$TRAVIS_BRANCH")
+    if [[ $? -ne 0 || ! $branch_ref ]]; then
+        err "failed to get $TRAVIS_BRANCH reference"
+        exit 1
+    fi
+
+    if [[ $head_ref != $branch_ref ]]; then
+        msg "HEAD ref ($head_ref) does not match $TRAVIS_BRANCH ref ($branch_ref)"
+        msg "someone may have pushed new commits before this build cloned the repo"
+        exit 1
+    fi
+}
 
 
 show_git_change() {
     echo "Show what was changed in the readme:"
     git diff --no-ext-diff README.md
+    git status
 }
 
 
@@ -14,6 +41,7 @@ setup_git() {
   echo "Changing git configuration to contain travis name and email"
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  git config --global push.default current
 }
 
 
@@ -23,25 +51,30 @@ commit_readme() {
   # https://docs.travis-ci.com/user/customizing-the-build/#skipping-a-build
 
   echo "Committing changes in the readme"
-  #git checkout -b master
+  git stash # So the checkout will not be affected with local changes done with the build, stash them first
+  git checkout ${TRAVIS_BRANCH}
+  git stash pop
   git add README.md
-  git commit --message "Updating README links (travis build: $TRAVIS_BUILD_NUMBER) [skip-ci]"
+  git commit --message "Updating README links (travis build: $TRAVIS_BUILD_NUMBER) [skip-ci]" # the [skip-ci] is important
 }
 
 
 push_updated_files() {
   echo "Setting the remote ${REPO} repository to contain GITHUB token (supplied as secret enviroment variable from travis)"
-  git remote add origin-travis https://truhlikfredy:${GITHUB_TOKEN}@github.com/${REPO}.git
+  #git remote add origin-travis https://${GITHUB_TOKEN}@github.com/${REPO}.git
 
   echo "Pushing the commit"
   echo "For the push to work on https a 2way auth is required"
   echo "https://help.github.com/articles/which-remote-url-should-i-use/"
 
-  git push --set-upstream origin-travis master
-  echo "Focing the head"
-  git push origin HEAD:origin-travis # reconcile detached HEAD workaround
-  echo "Push..."
-  git push
+  #git push --set-upstream origin-travis master
+  #echo "Focing the head"
+  #git push origin HEAD:origin-travis # reconcile detached HEAD workaround
+  #echo "Push..."
+  #git push
+
+  REMOTE="https://${GITHUB_TOKEN}@github.com/${REPO}.git"
+  git push "${REMOTE}" "${TRAVIS_BRANCH}"
   echo "Status..."
   git status
 }
@@ -52,6 +85,7 @@ cd $SCRIPT_PATH/..
 
 
 show_git_change
+check_if_repo_changed
 setup_git
 commit_readme
 push_updated_files
